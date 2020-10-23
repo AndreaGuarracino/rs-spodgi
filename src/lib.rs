6a867bdf97e3b460988e3e9b394fd399854fdcb1
AndreaGuarracino <andreaguarracino@outlook.com>
2020-10-23 23:57:42 +0200

    avoid panic when after the last step of a path there is a comma

--- src/lib.rs
+++ src/lib.rs
@@ -143,178 +143,180 @@ pub fn write_lines(lines: io::Lines<io::BufReader<File>>) -> Result<&'static str
                     //println!("<{}> a vg:Path .", tokens[1]);
                     triples.push(Triple::new(
                         &graph.create_uri_node_from_namespace_and_id(
-                            prefix_path, tokens[1]
+                            prefix_path, tokens[1],
                         ),
                         &a,
                         &graph.create_uri_node_from_namespace_and_id(
-                            prefix_vg, "Path"
-                        )
+                            prefix_vg, "Path",
+                        ),
                     ));
 
                     position_in_path = 0;
 
                     for (step_position, step) in tokens[2].split(",").enumerate() {
-                        //println!("\t<{}-step-{}> a vg:Step,\n\t\tfaldo:Region ;\n\t\tvg:rank {} ;\n\t\tvg:path <{}> ;", tokens[1], step_position + 1, step_position + 1, tokens[1]);
-                        let mut uri_string = "".to_string();
-                        uri_string.push_str(tokens[1]);
-                        uri_string.push_str("-");
-                        uri_string.push_str("region");
-                        uri_string.push_str("-");
-                        uri_string.push_str(&*format!("{}", step_position + 1));
+                        if !step.is_empty() {
+                            //println!("\t<{}-step-{}> a vg:Step,\n\t\tfaldo:Region ;\n\t\tvg:rank {} ;\n\t\tvg:path <{}> ;", tokens[1], step_position + 1, step_position + 1, tokens[1]);
+                            let mut uri_string = "".to_string();
+                            uri_string.push_str(tokens[1]);
+                            uri_string.push_str("-");
+                            uri_string.push_str("region");
+                            uri_string.push_str("-");
+                            uri_string.push_str(&*format!("{}", step_position + 1));
 
-                        let subject = graph.create_uri_node(&Uri::new(uri_string));
+                            let subject = graph.create_uri_node(&Uri::new(uri_string));
 
-                        triples.push(Triple::new(
-                            &subject,
-                            &a,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_vg, "Step"
-                            )
-                        ));
-                        triples.push(Triple::new(
-                            &subject,
-                            &a,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, "Region"
-                            ),
-                        ));
-                        triples.push(Triple::new(
-                            &subject,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_vg, "Rank"
-                            ),
-                            &graph.create_literal_node(
-                                format!("{}", step_position + 1)
-                            ),
-                        ));
-                        triples.push(Triple::new(
-                            &subject,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_vg, "Path"
-                            ),
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_path, tokens[1]
-                            ),
-                        ));
+                            triples.push(Triple::new(
+                                &subject,
+                                &a,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_vg, "Step",
+                                ),
+                            ));
+                            triples.push(Triple::new(
+                                &subject,
+                                &a,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, "Region",
+                                ),
+                            ));
+                            triples.push(Triple::new(
+                                &subject,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_vg, "Rank",
+                                ),
+                                &graph.create_literal_node(
+                                    format!("{}", step_position + 1)
+                                ),
+                            ));
+                            triples.push(Triple::new(
+                                &subject,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_vg, "Path",
+                                ),
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_path, tokens[1],
+                                ),
+                            ));
 
-                        //println!("{} {}", step, step.len());
-                        node = &step[..(step.len() - 1)];
-                        orientation = &step[(step.len() - 1)..];
+                            //println!("{} {}", step, step.len());
+                            node = &step[..(step.len() - 1)];
+                            orientation = &step[(step.len() - 1)..];
 
-                        node_term = "Node";
-                        node_orientation = "f";
-                        node_orientation_long = "ForwardStrandPosition";
+                            node_term = "Node";
+                            node_orientation = "f";
+                            node_orientation_long = "ForwardStrandPosition";
 
-                        if orientation == "-" {
-                            node_term = "reverseOfNode";
-                            node_orientation = "r";
-                            node_orientation_long = "ReverseStrandPosition";
-                        } else if orientation != "+" {
-                            return Err("the node orientation is not valid");
+                            if orientation == "-" {
+                                node_term = "reverseOfNode";
+                                node_orientation = "r";
+                                node_orientation_long = "ReverseStrandPosition";
+                            } else if orientation != "+" {
+                                return Err("the node orientation is not valid");
+                            }
+
+                            next_position_in_path = position_in_path + node_to_length.get(node).unwrap();
+
+                            beg = tokens[1].to_owned() + node_orientation + position_in_path.to_string().as_str();
+                            end = tokens[1].to_owned() + node_orientation + next_position_in_path.to_string().as_str();
+
+                            //println!("\t\tvg:{} node:{} ;\n\t\tfaldo:begin <{}> ;\n\t\tfaldo:end <{}> . ", node_term, node, beg, end);
+                            triples.push(Triple::new(
+                                &subject,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_vg, node_term,
+                                ),
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_node, node,
+                                ),
+                            ));
+
+                            let beg_node = &graph.create_uri_node(&Uri::new(beg));
+                            let end_node = &graph.create_uri_node(&Uri::new(end));
+                            triples.push(Triple::new(
+                                &subject,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, "begin",
+                                ),
+                                &beg_node,
+                            ));
+                            triples.push(Triple::new(
+                                &subject,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, "end",
+                                ),
+                                &end_node,
+                            ));
+
+                            //println!("\t\t<{}> a faldo:ExactPosition,\n\t\t\tfaldo:{} ;\n\t\t\tfaldo:position {} ;\n\t\t\tfaldo:reference <{}> . ", beg, node_orientation_long, position_in_path, tokens[1]);
+                            triples.push(Triple::new(
+                                &beg_node,
+                                &a,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, "ExactPosition",
+                                ),
+                            ));
+                            triples.push(Triple::new(
+                                &beg_node,
+                                &a,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, node_orientation_long,
+                                ),
+                            ));
+                            triples.push(Triple::new(
+                                &beg_node,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, "Position",
+                                ),
+                                &graph.create_literal_node(
+                                    format!("{}", position_in_path)
+                                ),
+                            ));
+                            triples.push(Triple::new(
+                                &beg_node,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, "reference",
+                                ),
+                                &graph.create_uri_node(&Uri::new(
+                                    tokens[1].to_string()
+                                )),
+                            ));
+
+                            //println!("\t\t<{}> a faldo:ExactPosition,\n\t\t\tfaldo:{} ;\n\t\t\tfaldo:position {} ;\n\t\t\tfaldo:reference <{}> . ", end, node_orientation_long, next_position_in_path, tokens[1]);
+                            triples.push(Triple::new(
+                                &end_node,
+                                &a,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, "ExactPosition",
+                                ),
+                            ));
+                            triples.push(Triple::new(
+                                &end_node,
+                                &a,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, node_orientation_long,
+                                ),
+                            ));
+                            triples.push(Triple::new(
+                                &end_node,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, "Position",
+                                ),
+                                &graph.create_literal_node(
+                                    format!("{}", next_position_in_path)
+                                ),
+                            ));
+                            triples.push(Triple::new(
+                                &end_node,
+                                &graph.create_uri_node_from_namespace_and_id(
+                                    prefix_faldo, "reference",
+                                ),
+                                &graph.create_uri_node(&Uri::new(
+                                    tokens[1].to_string()
+                                )),
+                            ));
+
+                            position_in_path = next_position_in_path;
                         }
-
-                        next_position_in_path = position_in_path + node_to_length.get(node).unwrap();
-
-                        beg = tokens[1].to_owned() + node_orientation + position_in_path.to_string().as_str();
-                        end = tokens[1].to_owned() + node_orientation + next_position_in_path.to_string().as_str();
-
-                        //println!("\t\tvg:{} node:{} ;\n\t\tfaldo:begin <{}> ;\n\t\tfaldo:end <{}> . ", node_term, node, beg, end);
-                        triples.push(Triple::new(
-                            &subject,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_vg, node_term
-                            ),
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_node, node
-                            ),
-                        ));
-
-                        let beg_node= &graph.create_uri_node(&Uri::new(beg));
-                        let end_node= &graph.create_uri_node(&Uri::new(end));
-                        triples.push(Triple::new(
-                            &subject,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, "begin"
-                            ),
-                            &beg_node,
-                        ));
-                        triples.push(Triple::new(
-                            &subject,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, "end"
-                            ),
-                            &end_node,
-                        ));
-
-                        //println!("\t\t<{}> a faldo:ExactPosition,\n\t\t\tfaldo:{} ;\n\t\t\tfaldo:position {} ;\n\t\t\tfaldo:reference <{}> . ", beg, node_orientation_long, position_in_path, tokens[1]);
-                        triples.push(Triple::new(
-                            &beg_node,
-                            &a,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, "ExactPosition"
-                            ),
-                        ));
-                        triples.push(Triple::new(
-                            &beg_node,
-                            &a,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, node_orientation_long
-                            ),
-                        ));
-                        triples.push(Triple::new(
-                            &beg_node,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, "Position"
-                            ),
-                            &graph.create_literal_node(
-                                format!("{}", position_in_path)
-                            ),
-                        ));
-                        triples.push(Triple::new(
-                            &beg_node,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, "reference"
-                            ),
-                            &graph.create_uri_node(&Uri::new(
-                                tokens[1].to_string()
-                            )),
-                        ));
-
-                        //println!("\t\t<{}> a faldo:ExactPosition,\n\t\t\tfaldo:{} ;\n\t\t\tfaldo:position {} ;\n\t\t\tfaldo:reference <{}> . ", end, node_orientation_long, next_position_in_path, tokens[1]);
-                        triples.push(Triple::new(
-                            &end_node,
-                            &a,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, "ExactPosition"
-                            ),
-                        ));
-                        triples.push(Triple::new(
-                            &end_node,
-                            &a,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, node_orientation_long
-                            ),
-                        ));
-                        triples.push(Triple::new(
-                            &end_node,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, "Position"
-                            ),
-                            &graph.create_literal_node(
-                                format!("{}", next_position_in_path)
-                            ),
-                        ));
-                        triples.push(Triple::new(
-                            &end_node,
-                            &graph.create_uri_node_from_namespace_and_id(
-                                prefix_faldo, "reference"
-                            ),
-                            &graph.create_uri_node(&Uri::new(
-                                tokens[1].to_string()
-                            )),
-                        ));
-
-                        position_in_path = next_position_in_path;
                     }
                 }
                 "H" => (), // Header
